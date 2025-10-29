3d4342dacb2c2c6a3059aaed1383101dcee7fd80
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-10-29 14:19:04 +0100

    Add Elasticsearch sync scheduler and manual trigger endpoint

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 from pyArango.connection import Connection
 from Utils.db import insert_json_db
 from Utils.home import home_data
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+from Utils.elastic_search import sync_to_elasticsearch
 import os
 
 app = Flask(__name__,template_folder='templates',static_folder='static')
@@ -39,12 +42,32 @@ init_db()  # Call the init_db function to initialize the db variable
 structure = None
 global data_dashboard
 data_dashboard = None
-#data_dashboard = dashboard(db, structure)
+# data_dashboard = dashboard(db, structure)
 
-from app.routes import doc_route, dashboard_route,reset_db, software_route, api_route, disambiguate_route, author_route, search_route, insert_json_route
+# --- Import routes ---
+from app.routes import doc_route, dashboard_route, reset_db, software_route, api_route, disambiguate_route, author_route
 
+# --- Scheduler for Elasticsearch Sync ---
+scheduler = BackgroundScheduler()
+scheduler.add_job(
+    func=sync_to_elasticsearch,
+    trigger="interval",
+    hours=24,
+    args=[db]  # Pass the ArangoDB connection to the job
+)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+# --- Flask Routes ---
 @app.route('/')
 def home():
     data = home_data(db)
     return render_template('pages/home.html', data=data[0])
 
+# Trigger Elasticsearch sync manually
+@app.route('/elastic_update')
+def run_task():
+    sync_to_elasticsearch(db)
+    return "Elastic executed manually!"
