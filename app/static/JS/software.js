d07f0f88df497642155263a46093e771bbc29994
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-11-04 16:40:31 +0100

    Fix software title display to properly decode URL-encoded software names

--- app/static/JS/software.js
+++ app/static/JS/software.js
@@ -16,7 +16,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
             .then(data => {
                 const title_block = document.createElement('div');
                 title_block.className = "titre-block";
-                title_block.innerHTML = `<h2 style="padding: 5px">${window.location.pathname.split('/', 2).pop()}/${data} test</h2>`;
+                const pathParts = window.location.pathname.split('/');
+                const softwareName = decodeURIComponent(pathParts.pop() || '');
+                title_block.innerHTML = `<h2 style="padding: 5px">${softwareName}/${data}</h2>`;
                 // Insert at the top of the div
                 div_block[0].insertBefore(title_block, div_block[0].firstChild);
             })
@@ -26,7 +28,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
         } else {
             const title_block = document.createElement('div');
             title_block.className = "titre-block";
-            title_block.innerHTML = `<h2 style="padding: 5px">${url_info}</h2>`;
+            const pathParts = window.location.pathname.split('/');
+            const softwareName = decodeURIComponent(pathParts.pop() || '');
+            title_block.innerHTML = `<h2 style="padding: 5px">${softwareName}/${data}</h2>`;
             // Insert at the top of the div
             div_block[0].insertBefore(title_block, div_block[0].firstChild);
         }
