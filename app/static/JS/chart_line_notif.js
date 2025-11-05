eaff8bd9d65c3d1c6bc4dfa51a981df60bba9f97
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-11-05 17:49:17 +0100

    bug correction for the POST in insert_json_route.py

--- app/static/JS/chart_line_notif.js
+++ app/static/JS/chart_line_notif.js
@@ -16,6 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 labels: labels,
                 datasets: [
                     {
+                        label: 'Notifications (last 30 days)',
+                        display: false,
                         data: notificationData,
                         fill: false,
                         spanGaps: true, // show gaps for null
