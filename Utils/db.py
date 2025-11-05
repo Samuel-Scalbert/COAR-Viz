8408c76619f9595cf0298470dfeb4c86ac15ecb0
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-11-05 14:48:46 +0100

    bug correction for the POST in insert_json_route.py

--- Utils/db.py
+++ Utils/db.py
@@ -97,12 +97,13 @@ def insert_json_db(data_path_json,data_path_xml,db):
     workbook = load_workbook(filename='./app/static/data/Logiciels_Blacklist_et_autres_remarques.xlsx')
     sheet = workbook.active
     data = []
+    print('la')
     for row in sheet.iter_rows(values_only=True):
         data.append(row)
     blacklist = []
     for row in data[1:]:
         blacklist.append(row[0])
-
+    print('la')
     urls_verified_sh = []
 
     # Create or retrieve collections
@@ -127,7 +128,7 @@ def insert_json_db(data_path_json,data_path_xml,db):
     dict_registered = {}
     dict_edge_author = {}
     new_file = False
-
+    print('lafin')
     for data_file_xml in tqdm(data_xml_list):
         print(f"Insertion of the file {data_file_xml}")
         file_path = f'{data_path_xml}/{data_file_xml}'
