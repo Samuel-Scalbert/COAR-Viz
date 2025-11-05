475fcaae723554e7d42964efcb12ee8c653b7c98
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-11-05 14:12:44 +0100

    bug correction for the POST in insert_json_route.py

--- app/routes/insert_json_route.py
+++ app/routes/insert_json_route.py
@@ -97,12 +97,12 @@ def insert_json():
         return jsonify({"error": f"Exception while saving JSON: {str(e)}"}), 500
 
     try:
-        insert_json_db("./app/static/data/json", "./app/static/data/xml", db)
         print('id:',hal_id)
         listinserted = db.AQLQuery(f'FOR hal_id in documents RETURN hal_id.file_hal_id', rawResults=True, batchSize=2000)
         print('list inserted:',listinserted[0])
         files_registered = db.AQLQuery(f'FOR hal_id in documents filter hal_id.file_hal_id == {hal_id} RETURN hal_id._id', rawResults=True, batchSize=2000)
         print('answer query:',files_registered)
+        insert_json_db("./app/static/data/json", "./app/static/data/xml", db)
         inserted = True
         if inserted == True:
             return jsonify({
